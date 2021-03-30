24314ce43ff7db9ee165b47978f2d0dcc9b9601c
Jonathan Yeong <jonathanyeong@gmail.com>
2021-03-30 15:37:04 -0700

    Use netlify adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,7 @@
 const { mdsvex } = require("mdsvex");
 const mdsvexConfig = require("./mdsvex.config.cjs");
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
+const netlify = require('@sveltejs/adapter-netlify');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,10 +13,8 @@ module.exports = {
 		mdsvex(mdsvexConfig),
 		sveltePreprocess()],
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: node(),
+
+		adapter: netlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
