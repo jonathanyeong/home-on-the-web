ef948a97d136e0afaa179d1a89a99e49739a66e5
Jonathan Yeong <jonathanyeong@gmail.com>
2021-04-25 20:52:31 -0700

    Add logo icon to navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,12 @@
+<script>
+  import Logo from '$lib/icons/Logo.svelte';
+</script>
+
 <nav>
+  <a href="/">
+    <span class="visually-hidden">Logo</span>
+    <Logo />
+  </a>
   <a href="/">Home</a>
   <a href="/garden">Digital Garden</a>
   <a href="/tutorials">Tutorials</a>
