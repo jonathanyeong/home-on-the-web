767ed4c82f2592d09f57f29053939947d12924c0
Jonathan Yeong <jonathanyeong@gmail.com>
2021-05-08 21:00:20 -0700

    Add the search component that contains the fuse library

---
+++ src/lib/Search.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Fuse from 'fuse.js'
+  export let data = [];
+  export let query = "";
+  export let options = {};
+  $: fuse = new Fuse(data, options);
+  $: if (data) fuse.setCollection(data);
+  $: if (query || data) {
+    result = fuse.search(query);
+    formatted = result.map((r) => {
+      return JSON.parse(r.item)
+    });
+  }
+
+</script>
