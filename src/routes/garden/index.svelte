deb9388a86f24e9ebd625c7164423394487ec97a
Jonathan Yeong <jonathanyeong@gmail.com>
2021-05-08 21:41:15 -0700

    First pass: Get search working!
    Search will be based on the tag + the search term. I also removed the Search
    component because the results weren't being passed back up to the parent. And I
    don't know why!

--- src/routes/garden/index.svelte
+++ src/routes/garden/index.svelte
@@ -17,10 +17,22 @@
 	import TagGroup from '$lib/TagGroup.svelte';
   export let posts;
 	export let tags;
+	import Fuse from 'fuse.js'
+
 	let activeTags = tags;
+	let query = "";
+	let options = { keys: ["title"] };
+	let result = []
+	let formatted = [];
 
 	$: postList = posts.filter(post => post.tags.some(t => activeTags.indexOf(t) >= 0));
-
+	$: fuse = new Fuse(postList, options);
+  $: if (postList) fuse.setCollection(postList);
+	$: if (query === "") formatted = postList
+  $: if (query !== "") {
+    result = fuse.search(query);
+		formatted = result.map((r) => { return r.item });
+  }
 	function handleTagUpdate(event) {
 		if (event.detail.activeTags.length === 0) {
 			activeTags = tags;
@@ -31,10 +43,11 @@
 </script>
 
 <h1>Welcome to the Garden</h1>
+<input placeholder="Search!" type="text" bind:value={query} />
 <p>Tag list is:</p>
-<TagGroup tags={tags} on:tagUpdate={handleTagUpdate}/>
+<TagGroup {tags} on:tagUpdate={handleTagUpdate}/>
 <ol>
-	{#each postList as post}
+	{#each formatted as post}
 		<li><a sveltekit:prefetch href="/garden/{post.slug}">{post.title} - {post.tags}</a></li>
 	{/each}
 </ol>
