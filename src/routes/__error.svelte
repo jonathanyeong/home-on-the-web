c46bf18ec56756de59c5cff5f98f6003c8881f1a
Jonathan Yeong <jonathanyeong@gmail.com>
2021-05-29 13:19:53 -0700

    Style the Albie picture!

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -3,7 +3,7 @@
 
   <p class="h4">This page doesn't exist! Here's a picture of my dog to make up for it</p>
 
-  <div>
+  <div class="profile-img__container">
     <img alt="Albie being cute and sleepy" src="https://res.cloudinary.com/jonathan-yeong/image/upload/v1622253869/personal-blog/IMG_1133-Edit_a4quos.png" />
   </div>
 </div>
@@ -14,15 +14,20 @@
     text-align: center;
   }
 
-  img {
-    width: 50%;
+  .profile-img__container {
+    max-width: 50%;
+    background-color: var(--heliotrope-gray);
+    border-radius: 5px;
+    margin: 0 auto;
+
+    img {
+      transform: translate(16px, -10px);
+      border-radius: 5px;
+      width: 100%;
+    }
   }
 
   p.h4 {
     margin-bottom: 32px;
-  }
-
-  .title {
-    color: var(--heliotrope-gray);
   }
 </style>
