3ff0470464f613bb3fabf33c4508e4c085ebe80e
Jonathan Yeong <jonathanyeong@gmail.com>
2021-05-28 19:11:47 -0700

    Add error page to site

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<div class="error-layout">
+<h1 class="title">OH NO!</h1>
+
+<p class="h4">This page doesn't exist! Here's a picture of my dog to make up for it</p>
+
+<div>
+  <img alt="Albie being cute and sleepy" src="https://res.cloudinary.com/jonathan-yeong/image/upload/v1622253869/personal-blog/IMG_1133-Edit_a4quos.png" />
+</div>
+</div>
+
+
+<style lang="scss">
+  .error-layout {
+    text-align: center;
+  }
+
+  img {
+    width: 50%;
+
+  }
+
+  p.h4 {
+    margin-bottom: 32px;
+  }
+</style>
