3674f0e1072e1f05943145713dc193191bf4cce3
Jonathan Yeong <jonathanyeong@gmail.com>
2021-05-28 19:18:37 -0700

    Add some colours to the error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,11 +1,11 @@
 <div class="error-layout">
-<h1 class="title">OH NO!</h1>
+  <h1 class="title">OH NO!</h1>
 
-<p class="h4">This page doesn't exist! Here's a picture of my dog to make up for it</p>
+  <p class="h4">This page doesn't exist! Here's a picture of my dog to make up for it</p>
 
-<div>
-  <img alt="Albie being cute and sleepy" src="https://res.cloudinary.com/jonathan-yeong/image/upload/v1622253869/personal-blog/IMG_1133-Edit_a4quos.png" />
-</div>
+  <div>
+    <img alt="Albie being cute and sleepy" src="https://res.cloudinary.com/jonathan-yeong/image/upload/v1622253869/personal-blog/IMG_1133-Edit_a4quos.png" />
+  </div>
 </div>
 
 
@@ -16,10 +16,13 @@
 
   img {
     width: 50%;
-
   }
 
   p.h4 {
     margin-bottom: 32px;
+  }
+
+  .title {
+    color: var(--heliotrope-gray);
   }
 </style>
