bf1a89e1e940174857f47483a66d47e8f8379ee7
Jonathan Yeong <jonathanyeong@gmail.com>
2021-06-01 17:13:35 -0700

    Update rss feed

--- src/routes/rss.xml.js
+++ src/routes/rss.xml.js
@@ -3,24 +3,25 @@ const siteUrl = 'https://jonathanyeong.com';
 const renderXmlRssFeed = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 <channel>
-    <title><![CDATA[Jonathan's Blog]]></title>
-    <link>${siteUrl}</link>
-  <description><![CDATA[A developer's blog. Might be useful. Maybe.]]></description>
-  <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    <image>
-        <url>${siteUrl}/profile-pic-small.jpg</url>
-        <title><![CDATA[Jonathan's Blog]]></title>
-        <link>${siteUrl}</link>
-    </image>
-    ${posts.map(post => `
-        <item>
-            <title>${post.title}</title>
-      <link>${siteUrl}/${post.slug}</link>
-      <guid isPermaLink="false">${siteUrl}/${post.slug}</guid>
-            <description><![CDATA[${post.description}]]></description>
-            <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-        </item>
-    `).join('\n')}
+  <title><![CDATA[Jonathan Yeong's Blog]]></title>
+  <subtitle><![CDATA[A digital garden focused around all things developer]]></subtitle>
+  <link href=${siteUrl} rel="self"/>
+  <link href=${siteUrl}/>
+  <description><![CDATA[ I write about a variety topics around being a developer. With technical articles focused around Ruby, Elixir, Javascript, and their respective ecosystems.]]></description>
+  	<author>
+		<name>Jonathan Yeong</name>
+		<email>hello@jonathanyeong.com</email>
+	</author>
+  ${posts.map(post => `
+      <item>
+          <title>${post.title}</title>
+          <link href=${siteUrl}/${post.slug} />
+          <description><![CDATA[${post.description}]]></description>
+          <updated>${new Date(post.lastUpdatedDate).toUTCString()}</updated>
+          <pubDate>${new Date(post.createdAtDate).toUTCString()}</pubDate>
+          <id>${post.slug}</id>
+      </item>
+  `).join('\n')}
 </channel>
 </rss>`;
 
@@ -32,19 +33,19 @@ import tutorials from './tutorials/_tutorials'
  */
 export async function get() {
   const allPlants = await plants();
-  const allTutorials = await tutorials();
-  const allContent = {...allPlants, ...allTutorials}
+  // const allTutorials = await tutorials();
+  // const allContent = {...allPlants, ...allTutorials}
 
-  const posts = Object.keys(allContent).map((slug) => {
-    return {
-        title: allContent[slug].title,
-        date: allContent[slug].date,
-        description: allContent[slug].description,
-        slug: slug,
-      };
-  })
+  // const posts = Object.values(allPlants).map((slug) => {
+  //   return {
+  //       title: allContent[slug].title,
+  //       date: allContent[slug].date,
+  //       description: allContent[slug].description,
+  //       slug: slug,
+  //     };
+  // })
 
-  const feed = renderXmlRssFeed(posts);
+  const feed = renderXmlRssFeed(Object.values(allPlants));
 
 
   return {
