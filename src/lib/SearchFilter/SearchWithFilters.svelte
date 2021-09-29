11f202768ed87c0860b17ecbf5657ebd019df8ef
Jonathan Yeong <jonathanyeong@gmail.com>
2021-09-28 17:14:46 -0700

    Add chronological sort method
    Uncomment out code

--- src/lib/SearchFilter/SearchWithFilters.svelte
+++ src/lib/SearchFilter/SearchWithFilters.svelte
@@ -1,6 +1,8 @@
 <script>
 	import SearchBar from '$lib/SearchFilter/SearchBar.svelte';
+	import { chronologicallySorted } from '$lib/chronologicalSort';
 	import Fuse from 'fuse.js';
+
 	export let posts = [];
 	export let formattedResults = [];
 	let results = []
@@ -20,20 +22,20 @@
 	const fuse = new Fuse(posts, options, postsIndex);
 
 	let query = "";
+	let tagToggle = false;
 
   $: if (query !== "") {
 		results = fuse.search(query).map(r => r.item);
 		formattedResults = results
   }
 
-	// $: if (query === "" && !tagToggle) {
-	// 	formattedResults = chronologicallySorted(posts);
-	// }
+	$: if (query === "" && !tagToggle) {
+		formattedResults = chronologicallySorted(posts);
+	}
 
-	// $: if (query === "" && tagToggle) {
-	// 	formattedResults = chronologicallySorted(activeTagPosts);
-	// }
-
+	$: if (query === "" && tagToggle) {
+		formattedResults = chronologicallySorted(activeTagPosts);
+	}
 </script>
 
 <SearchBar bind:query={query} />
